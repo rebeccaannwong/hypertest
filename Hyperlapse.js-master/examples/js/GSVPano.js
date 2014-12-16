bbb32832be661db6e421323c16f7385f8c7ebd10
Rebecca Ann Wong <rebecca.ann.wong@gmail.com>
2014-12-16 16:50:58 -0500

    corrected errors on simple.html - added blank image in pano and moved varout of function init

--- Hyperlapse.js-master/examples/js/GSVPano.js
+++ Hyperlapse.js-master/examples/js/GSVPano.js
@@ -17,27 +17,27 @@ GSVPANO.PanoLoader = function (parameters) {
 		copyright = '',
 		onSizeChange = null,
 		onPanoramaLoad = null;
-		
+
 	this.setProgress = function (p) {
-	
+
 		if (this.onProgress) {
 			this.onProgress(p);
 		}
-		
+
 	};
 
 	this.throwError = function (message) {
-	
+
 		if (this.onError) {
 			this.onError(message);
 		} else {
 			console.error(message);
 		}
-		
+
 	};
 
 	this.adaptTextureToZoom = function () {
-	
+
 		var w = 416 * Math.pow(2, _zoom),
 			h = (416 * Math.pow(2, _zoom - 1));
 		_canvas.width = w;
@@ -47,55 +47,58 @@ GSVPANO.PanoLoader = function (parameters) {
 	};
 
 	this.composeFromTile = function (x, y, texture) {
-	
+
 		_ctx.drawImage(texture, x * 512, y * 512);
 		_count++;
-		
+
 		var p = Math.round(_count * 100 / _total);
 		this.setProgress(p);
-		
+
 		if (_count === _total) {
 			this.canvas = _canvas;
 			if (this.onPanoramaLoad) {
 				this.onPanoramaLoad();
 			}
 		}
-		
+
 	};
 
 	this.composePanorama = function (panoId) {
-	
+
 		this.setProgress(0);
 		console.log('Loading panorama for zoom ' + _zoom + '...');
-		
+
 		var w = (_zoom==3) ? 7 : Math.pow(2, _zoom),
 			h = Math.pow(2, _zoom - 1),
 			self = this,
 			url,
 			x,
 			y;
-				
+
 		_count = 0;
 		_total = w * h;
-	
+
 		for( y = 0; y < h; y++) {
 			for( x = 0; x < w; x++) {
 				url = 'http://maps.google.com/cbk?output=tile&panoid=' + panoId + '&zoom=' + _zoom + '&x=' + x + '&y=' + y + '&' + Date.now();
-				(function (x, y) { 
+				(function (x, y) {
 					var img = new Image();
 					img.addEventListener('load', function () {
 						self.composeFromTile(x, y, this);
 					});
-					img.crossOrigin = '';
+					img.addEventListener('error', function () {
+						self.composeFromTile(x, y, new Image());
+					});
+					img.crossOrigin = 'anonymous';
 					img.src = url;
 				})(x, y);
 			}
 		}
-		
+
 	};
-	
+
 	this.load = function (location, callback) {
-	
+
 		console.log('Load for', location);
 		var self = this;
 		_panoClient.getPanoramaByLocation(location, 50, function (result, status) {
@@ -118,9 +121,9 @@ GSVPANO.PanoLoader = function (parameters) {
 				callback();
 			}
 		});
-		
+
 	};
-	
+
 	this.setZoom = function( z ) {
 		_zoom = z;
 		this.adaptTextureToZoom();
